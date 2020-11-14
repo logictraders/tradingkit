b35bb0efa902d8bc53976fb28a60418bbe082f75
Lluis Santos <lluis@qbitartifacts.com>
2020-11-14 11:21:02 +0100

    fix(packaging): fixed packaging (V)

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 import setuptools
-
 setuptools.setup(
       name="tradingkit",
-      version="1.1.8",
+      version="1.1.9",
       author="QBit Artifacts, SL",
       author_email="lluis@logictraders.com",
-      license="Propietary",
-      description="Criptocurrency trading framework",
+      license="MIT",
+      description="Criptocurrency trading and backtesting framework",
       long_description=open("README.md", "r").read(),
       long_description_content_type="text/markdown",
       url="https://github.com/logictraders/tradingkit",
@@ -19,18 +18,23 @@ setuptools.setup(
             ]
       },
       classifiers=[
-            "Development Status :: 2 - Pre-Alpha",
+            "Development Status :: 3 - Alpha",
             "Programming Language :: Python",
             "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Environment :: Console",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
             "Intended Audience :: Developers",
-            "Topic :: Internet",
+            "Topic :: Office/Business",
             "Topic :: Software Development",
             "Topic :: Software Development :: Build Tools",
             "Topic :: Software Development :: Libraries",
-            "Topic :: Software Development :: Libraries :: Python Modules"
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "Topic :: Software Development :: Libraries :: Application Frameworks"
       ],
       install_requires=[line.strip() for line in open("requirements.txt").readlines() if len(line) > 1]
 )
+
