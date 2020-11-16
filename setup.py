c610629f063785bf84375ba61aa01b4430e17aaf
Lluis Santos <lluis@qbitartifacts.com>
2020-11-16 17:13:36 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 setuptools.setup(
       name="tradingkit",
-      version="1.1.20",
+      version="1.2.0",
       author="QBit Artifacts, SL",
       author_email="lluis@logictraders.com",
       license="MIT",
