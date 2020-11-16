21fe3d5719f80ab6c2017bca1e93f93be0d4c1e6
Lluis Santos <lluis@qbitartifacts.com>
2020-11-16 12:20:03 +0100

    fix(packaging): added init to config package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 setuptools.setup(
       name="tradingkit",
-      version="1.1.18",
+      version="1.1.19",
       author="QBit Artifacts, SL",
       author_email="lluis@logictraders.com",
       license="MIT",
