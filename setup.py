607b9fc3aba15d315772edfe67b761b539940e70
Lluis Santos <lluis@qbitartifacts.com>
2020-11-16 10:51:32 +0100

    fix(packaging): changed from setuptools to setuptools_git

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 setuptools.setup(
       name="tradingkit",
-      version="1.1.16",
+      version="1.1.17",
       author="QBit Artifacts, SL",
       author_email="lluis@logictraders.com",
       license="MIT",
