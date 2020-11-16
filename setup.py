87d88e1fc486d26c7cb569a4c8560278c0e218ef
Lluis Santos <lluis@qbitartifacts.com>
2020-11-16 10:36:55 +0100

    fix(packaging): fixed packaging (XI)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 setuptools.setup(
       name="tradingkit",
-      version="1.1.15",
+      version="1.1.16",
       author="QBit Artifacts, SL",
       author_email="lluis@logictraders.com",
       license="MIT",
@@ -11,7 +11,7 @@ setuptools.setup(
       url="https://github.com/logictraders/tradingkit",
       packages=setuptools.find_namespace_packages(where="src", include=['tradingkit.*']),
       package_dir={"": "src"},
-      package_data={"tradingkit": ["config/*"]},
+      include_package_data=True,
       entry_points={
             "console_scripts": [
                   "tk = tradingkit.cli.cli:CLI.main"
