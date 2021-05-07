54308e182b173c77503917662dba850481fa45ab
Lluis Santos <lluis@qbitartifacts.com>
2021-05-07 18:11:06 +0200

    chore(release): 1.6.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 setuptools.setup(
       name="tradingkit",
-      version="1.6.6",
+      version="1.6.7",
       author="QBit Artifacts, SL",
       author_email="lluis@logictraders.com",
       license="MIT",
