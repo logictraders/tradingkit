1aa1ce6f5be6c767a65a79536e99414de16f10f1
iulian <iulian@qbitartifacts.com>
2021-06-16 11:35:35 +0200

    feat(bitmex_backtest):

--- src/tradingkit/exchange/bitmex_backtest.py
+++ src/tradingkit/exchange/bitmex_backtest.py
@@ -16,7 +16,7 @@ class BitmexBacktest(TestEX):
         pass
 
     def __init__(self, params=None):
-        super().__init__({'balance': {'USD': 0, 'BTC': 10}, 'fees': {'maker': -0.00025, 'taker': 0.00075}})
+        super().__init__(params)
         self.position = {"currentQty": 0,  # currentQty
                          "homeNotional": 0,  # homeNotional base value (disabled)
                          "avgEntryPrice": 0,
