772b86ecdf34f1e30020a38dbdb4af5fcc86d762
iulian <iulian@qbitartifacts.com>
2022-01-20 20:54:49 +0100

    fix(bridge_exchange): MDD update frequency

--- src/tradingkit/exchange/bridge_exchange.py
+++ src/tradingkit/exchange/bridge_exchange.py
@@ -320,8 +320,8 @@ class BridgeExchange(Publisher, Subscriber, Exchange):
 
             price = event.payload['close']
             base, quote = self.symbol.split('/')
-            balances = self.fetch_balance()
-            balances = balances['free'] if balances['free'][base] else balances['total']
+            _balances = self.fetch_balance()
+            balances = _balances['free'] if _balances['free'][base] else _balances['total']
 
             base_balance = balances[base] if base in balances else 0
             quote_balance = balances[quote] if quote in balances else 0
@@ -330,6 +330,7 @@ class BridgeExchange(Publisher, Subscriber, Exchange):
             base_equity = base_balance + quote_balance / price
 
             self.balance_history.append(base_equity)
+            self.calculate_max_drawdown(_balances['total'][base], _balances['total'][quote])
 
 
     def get_sharpe_ratio(self):
