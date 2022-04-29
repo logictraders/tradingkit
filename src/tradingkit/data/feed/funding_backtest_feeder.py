d3701e3433766f8be8800b95f599755421814431
iulian <iulian@qbitartifacts.com>
2022-04-29 12:38:00 +0200

    feat(multi_exchange): added sleep to test

--- src/tradingkit/data/feed/funding_backtest_feeder.py
+++ src/tradingkit/data/feed/funding_backtest_feeder.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import time
 from datetime import datetime
 import csv
 from dateutil.relativedelta import relativedelta
@@ -27,6 +28,7 @@ class FundingBacktestFeeder(BacktestFeeder, Publisher):
             if since.timestamp() <= trade['timestamp'] / 1000 < to.timestamp():
                 self.dispatch_founding_rate(trade, exchange, base, quote)
                 trade['exchange'] = 'bitmex'
+                time.sleep(10000)
                 self.dispatch(Trade(trade))
 
     def dispatch_founding_rate(self, trade, exchange, base, quote):
