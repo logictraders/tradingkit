61ead7cafccfd4d308f7864d25715e3ef690ca2f
Lluis Santos <lluis@ngynx.com>
2022-10-26 21:43:04 +0200

    fix(setup): bumped minimum python version to 3.9

--- setup.py
+++ setup.py
@@ -21,8 +21,6 @@ setuptools.setup(
             "Development Status :: 3 - Alpha",
             "Programming Language :: Python",
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Environment :: Console",
             "License :: OSI Approved :: MIT License",
